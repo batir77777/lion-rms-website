94d354f9c3359f77d209bc18268011afde956a0f
batir77777 <batirturakulov7@gmail.com>
2026-08-01 07:11:15 +0100

    PR8A: file-validation fixture configs

---
+++ tests/fixtures-config/download-missing-file.velite.config.ts
@@ -0,0 +1,2 @@
+import { makeFixtureConfig } from "./make-config";
+export default makeFixtureConfig("content/__fixtures__/download-missing-file", "download-missing-file");
---
+++ tests/fixtures-config/download-no-delivery.velite.config.ts
@@ -0,0 +1,2 @@
+import { makeFixtureConfig } from "./make-config";
+export default makeFixtureConfig("content/__fixtures__/download-no-delivery", "download-no-delivery");
